6474fb3a1a507c593ff0ee9d208ad4383a608d99
unconst <jake@bittensor.com>
2023-07-21 22:26:11 -0500

    add run command

--- run.py
+++ run.py
@@ -1,3 +1,20 @@
+# The MIT License (MIT)
+# Copyright © 2023 Yuma Rao
+
+# Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated
+# documentation files (the “Software”), to deal in the Software without restriction, including without limitation
+# the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software,
+# and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+
+# The above copyright notice and this permission notice shall be included in all copies or substantial portions of
+# the Software.
+
+# THE SOFTWARE IS PROVIDED “AS IS”, WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO
+# THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+# THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
+# OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+# DEALINGS IN THE SOFTWARE.
+
 import os
 import sys
 import time
@@ -26,11 +43,12 @@ class Process:
         self.start()
 
 def git_has_changes():
-    bt.logging.success('Checking git changes on origin/main' )
+    current_branch = subprocess.check_output(['git', 'rev-parse', '--abbrev-ref', 'HEAD']).strip().decode('utf-8')
+    bt.logging.success(f'Checking git changes on {current_branch}' )
     result = subprocess.run(['git', 'fetch'], stdout=subprocess.DEVNULL, stderr=subprocess.STDOUT)
     if result.returncode != 0:
         raise RuntimeError('Error fetching git updates')
-    result = subprocess.run(['git', 'diff', 'HEAD', 'origin/main'], stdout=subprocess.PIPE)
+    result = subprocess.run(['git', 'diff', current_branch, 'origin/'+current_branch], stdout=subprocess.PIPE)
     return result.stdout != b''
 
 def git_pull():
@@ -45,12 +63,20 @@ def main():
     p.start()
     try:
         while True:
+
+            # Check if there are git changes on this local branch.
             if git_has_changes():
                 bt.logging.success('Changes detected. Pulling updates and restarting...')
                 git_pull()
                 p.restart()
-            time.sleep(10)
+            else:
+                bt.logging.success('No changes detected. Continuing.')
+
+            # Wait 30 seconds.
+            time.sleep(30)
+
     except KeyboardInterrupt:
+        # Log and stop both processes.
         bt.logging.success('Interrupted by user. Exiting.')
         p.stop()
 
