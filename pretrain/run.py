c04f9fd66d5a32fa3a27f88250c8b4564b32eb2c
unconst <jake@bittensor.com>
2023-07-22 13:39:12 -0500

    log total accumulated examples

--- pretrain/run.py
+++ pretrain/run.py
@@ -103,7 +103,6 @@ def run( self ):
 
         # Train on epoch.
         for acc_step, batch in enumerate( self.dataloader ):
-            total_accumulated_examples = total_accumulation_steps * self.config.bs
             num_accumulated = (acc_step +1) % self.config.n_acc * self.config.bs
             total_to_accumulate = self.config.n_acc * self.config.bs
             bt.logging.success(f'Step: { num_accumulated }/{ total_to_accumulate }, Accumulations: {total_accumulation_steps}, Training: {total_training_steps}, Epoch: {total_epoch_steps}')
@@ -155,7 +154,7 @@ def run( self ):
             
             # Log all params. 
             total_accumulation_steps += 1
-            total_accumulated_samples += total_accumulation_steps * self.config.bs
+            total_accumulated_samples += self.config.bs
             torch.cuda.empty_cache() # Clear cache if existent.
             self.wandb.log({ 'block': self.subtensor.block })
             self.wandb.log({ 'total_accumulated_samples': total_accumulated_samples })
