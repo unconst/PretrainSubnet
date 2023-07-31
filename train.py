c69bb36f68af352f6e7ec067e6765fe9c371ef84
unconst <jake@bittensor.com>
2023-07-31 10:04:39 -0500

    reset

---
+++ train.py
@@ -0,0 +1,165 @@
+# Imports
+import os
+import sys
+import torch
+import wandb
+import random
+import argparse
+import torch.nn as nn
+import bittensor as bt
+from datasets import load_dataset
+from torch.utils.data import DataLoader
+from transformers import GPT2LMHeadModel, GPT2Config, GPT2Tokenizer, AdamW, get_linear_schedule_with_warmup
+
+# Pull in training utils.
+import utils
+
+def parse_arguments():
+    parser = argparse.ArgumentParser()
+    parser.add_argument( '--lr', type=float, default = 5e-5, help='Training learning rate.')
+    parser.add_argument( '--bs', type=int, default = 1, help='Training batch size')
+    parser.add_argument( '--sl', type=int, default = 512, help='Training sequence length')
+    parser.add_argument( '--wandb', action="store_true", default = False, help='Turn on wandb')
+    parser.add_argument( '--max_k', type=int, default = 1, help='Max number of gradients to merge.')
+    parser.add_argument( '--max_steps', type=int, default = 50000, help='Max training steps.')
+    parser.add_argument( '--steps_per_log', type=int, default = 1, help='Number of steps per log.')
+    parser.add_argument( '--steps_per_sync', type=int, default = 10, help='Number of steps per chain sync.')
+    parser.add_argument( '--num_warmup', type=int, default = 2000, help='Scheduler warm up steps.')
+    parser.add_argument( '--accs_per_step', type=int, default= 3, help='Number of training accumulation steps.')
+    parser.add_argument( '--netuid', type = int, default = 97, help="The chain subnet uid." )
+    parser.add_argument( '--chain_endpoint', type = str, default = "wss://test.finney.opentensor.ai", help="The chain endpoint to connect with." )
+    bt.subtensor.add_args( parser )
+    bt.wallet.add_args( parser )
+    bt.axon.add_args( parser )
+    bt.logging.add_args( parser )
+    return bt.config( parser )
+
+config = parse_arguments()
+pass
+
+
+# Setup model and tokenizer
+def setup_model_and_tokenizer():
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    from transformers import GPT2LMHeadModel, GPT2Config, GPT2Tokenizer, AdamW, get_linear_schedule_with_warmup
+    tokenizer = GPT2Tokenizer.from_pretrained('gpt2')
+    tokenizer.pad_token = tokenizer.eos_token
+    model = GPT2LMHeadModel(GPT2Config()).to(device)
+    model.train()
+    return model, tokenizer, device
+
+model, tokenizer, device = setup_model_and_tokenizer()
+pass
+
+# Load dataloader
+def load_dataloader():
+    def tokenize_function(examples):
+        return tokenizer(examples["text"], truncation = True, padding = "max_length", max_length = config.sl, return_tensors = "pt")
+    dataset = load_dataset("togethercomputer/RedPajama-Data-1T", 'default', split='train', streaming=True)
+    dataset = dataset.shuffle(buffer_size = config.bs * 4, seed=42)
+    tokenized_dataset = dataset.map( tokenize_function, batched=True )
+    dataloader = DataLoader( tokenized_dataset, batch_size = config.bs)
+    return dataloader
+
+dataloader = load_dataloader()
+pass
+
+
+# Get optimized and scheduler
+optimizer = torch.optim.AdamW (model.parameters(), lr = config.lr)
+scheduler = get_linear_schedule_with_warmup(optimizer, num_warmup_steps=config.num_warmup, num_training_steps=config.max_steps)  # assuming total steps
+pass
+
+# Set up wandb
+if config.wandb:
+    wandb = wandb.init(
+        anonymous = "allow",
+        project = "openpretrain",
+        entity = "opentensor-dev",
+        config = config,
+        mode = "online"
+    )
+
+# Set up Bittensor
+bt.logging( config = config )
+wallet = bt.wallet( config = config ).create_if_non_existent()
+subtensor = bt.subtensor( chain_endpoint = config.chain_endpoint  )
+dendrite = bt.dendrite( wallet = wallet )
+axon = bt.axon( wallet = wallet, config = config )
+metagraph = subtensor.metagraph( config.netuid )
+
+# Set up chain connection.
+def chain_sync():
+    subtensor.register( wallet = wallet, netuid = config.netuid )
+    metagraph = subtensor.metagraph( config.netuid )
+    my_uid = metagraph.hotkeys.index( wallet.hotkey.ss58_address )
+    axon.serve( netuid = config.netuid, subtensor = subtensor )
+    if subtensor.block - metagraph.last_update[ my_uid ] > 50:
+        subtensor.set_weights( netuid = config.netuid, wallet = wallet, uids = [my_uid], weights = [1.0] )
+    metagraph = subtensor.metagraph( config.netuid )
+chain_sync()
+
+# Set up synapse.
+def get_grads( synapse: utils.GetGrads ) -> utils.GetGrads:
+    synapse.grads = { name: bt.tensor(parameter.grad.clone()) for name, parameter in model.named_parameters() if parameter.grad is not None }
+    return synapse
+axon.attach( get_grads ).start()
+
+# Set up dendrite get grads.
+def merge_random():
+    # Query random available axon.
+    available = [ metagraph.axons[uid] for uid in metagraph.uids if subtensor.block - metagraph.last_update[uid] < 100 ]
+    axon = random.choice( available )
+
+    # Query axon and get grads.
+    grad_dict = dendrite.query( axon, utils.GetGrads() )
+
+    # Check if it is valid.
+    if utils.is_valid_grad_dict( model, grad_dict ):
+
+        # Apply grad to model.
+        utils.apply_grads_to_model( model, grad_dict )
+
+# training loop
+step = 0
+accumulation_counter = 0
+for epoch in range(3):
+    print(f'Epoch {epoch + 1}/{3}')
+    for batch in dataloader:
+        
+        # Forward pass.
+        outputs = model(
+            input_ids = batch["input_ids"].to(device), 
+            attention_mask = batch["attention_mask"].to(device),
+            labels = batch["input_ids"].to(device)
+        ) 
+        
+        # Backward pass
+        loss = outputs.loss / config.accs_per_step
+        loss.backward()
+
+        # Merge gradients with a random peer.
+        merge_random()
+        
+        # Accumulate across batches.
+        accumulation_counter += 1
+        if accumulation_counter % config.accs_per_step == 0:
+
+            # Apply gradient step.
+            optimizer.step()
+            scheduler.step() 
+            optimizer.zero_grad()
+            
+            # Log state to terminal and wandb.
+            if step % config.steps_per_log == 0:
+                perplexity = torch.exp(loss * config.accs_per_step).item()
+                loss = loss * config.accs_per_step
+                bt.logging.info(f'Step {step}, Loss {loss}, Perplexity {perplexity}')
+                if config.wandb: wandb.log( {'step': step, 'loss': loss, 'perplexity': perplexity } )
+
+            # Sync chain state.
+            if step % config.steps_per_sync == 0:
+                chain_sync()
+
+            # Increment step.
+            step += 1
---
+++ utils.py
@@ -0,0 +1,361 @@
+# The MIT License (MIT)
+# Copyright © 2023 Yuma Rao
+
+# Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated
+# documentation files (the “Software”), to deal in the Software without restriction, including without limitation
+# the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software,
+# and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+
+# The above copyright notice and this permission notice shall be included in all copies or substantial portions of
+# the Software.
+
+# THE SOFTWARE IS PROVIDED “AS IS”, WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO
+# THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+# THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
+# OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+# DEALINGS IN THE SOFTWARE.
+
+import torch
+import typing
+import bittensor as bt
+from types import SimpleNamespace
+
+# Protocol Definition to get Gradients
+class GetGrads( bt.Synapse ):
+    """
+    The GetGrads class is used to get the gradients of the model.
+    It subclasses the bittensor Synapse.
+    """
+    # Gradients per variable in the model.
+    grads: typing.Optional[typing.Dict[str, bt.Tensor]] = None
+
+    # Define deserialization function
+    def deserialize( self ) -> typing.Dict[str, torch.Tensor]:
+        """
+        Deserialize method converts the Bittensor gradients to Pytorch tensors.
+
+        Returns:
+        Dictionary with gradient tensors.
+        """
+        return { name: g.tensor() for name, g in self.grads.items() } if self.grads != None else None
+    
+
+def apply_grads_to_model( model: torch.nn.Module, grad_dicts: typing.Dict[str, torch.Tensor] ):
+    """
+    This function applies the averaged gradients from the input gradient dictionary to 
+    the parameters of the model.
+
+    Parameters:
+        avg_valid_grads_dict (dict): A gradient dictionary with averaged gradients. This dictionary
+            typically contains mappings from parameter names to corresponding gradients.
+    """
+
+    # Apply the averaged gradients to the model's parameters
+    for name, param in model.named_parameters():
+        # Only apply the gradients if the parameter exists in the averaged gradients dictionary
+        if name in grad_dicts:
+            # If the parameter already has a gradient, add the averaged gradient to it
+            # Otherwise, assign the averaged gradient as the parameter's gradient
+            if param.grad is not None:
+                param.grad += grad_dicts[name].to( model.device )
+            else:
+                param.grad = grad_dicts[name].clone().to( model.device )
+
+def average_grad_dicts( model: torch.nn.Module, grad_dicts: typing.List[typing.Dict[str, torch.Tensor]], average: bool = False) -> typing.Dict[str, torch.Tensor]:
+    """
+    This function averages the gradients from a list of valid gradient dictionaries and 
+    returns a new gradient dictionary with the averaged gradients.
+
+    Parameters:
+        valid_grad_dicts (list): A list of valid gradient dictionaries. Each gradient dictionary
+            typically contains mappings from layer names to corresponding gradients.
+        average (bool): If true, the gradients are averaged, rather summed.
+    Returns:
+        dict: A gradient dictionary with averaged gradients.
+    """
+    # Build average
+    avg_valid_grads_dict = {}
+
+    # Iterate over the keys in the model's state_dict (which should correspond to the parameter names)
+    for key in model.state_dict().keys():
+        # Find the gradients for the current parameter across all the returned dictionaries
+        all_grads = [grad_dict[key].to(model.device) for grad_dict in grad_dicts if key in grad_dict]
+
+        # If there are no gradients for this parameter, skip it
+        if len(all_grads) == 0:
+            continue
+
+        # Initialize an empty tensor on the correct device to hold the sum of gradients
+        grad_sum = torch.zeros_like(model.state_dict()[key], device=model.device)
+
+        # Add each grad tensor to the sum (this replaces stacking and taking the mean)
+        for grad in all_grads:
+            grad_sum += grad.to(model.device)
+
+        # Divide the sum by the number of gradients to get the average (note: this is an in-place operation)
+        if average:
+            grad_sum.div_(len(all_grads))
+
+        # Assign the averaged gradient to the averaged gradients dictionary
+        avg_valid_grads_dict[key] = grad_sum.to(model.device)
+
+        # Delete all_grads and grad_sum to free up memory
+        del all_grads
+        del grad_sum
+
+    return avg_valid_grads_dict
+
+
+def is_valid_grad_dict( model: torch.nn.Module, grad_dict ) -> bool:
+    """
+    This function checks whether a given grad_dict is valid for a PyTorch model.
+    The grad_dict is valid if:
+        - it is a dictionary and not empty
+        - all keys in the grad_dict match the keys in the model's state_dict
+        - all values in the grad_dict are float tensors
+        - all elements of the tensors are finite
+        - the shapes of tensors in the grad_dict match with corresponding tensors in the model's state_dict
+
+    Parameters:
+        grad_dict (dict): The gradient dictionary to validate. This dict typically contains
+            mappings from layer names to corresponding gradients.
+
+    Returns:
+        bool: True if the grad_dict is valid, False otherwise.
+    """
+    # Check if the grad_dict is a non-empty dictionary
+    if grad_dict is None or not isinstance(grad_dict, dict) or len(grad_dict.keys()) == 0:
+        bt.logging.warning(f'Invalid grad_dict: Is None, empty or not a dict: {grad_dict}')
+        return False
+
+    # Iterate over all keys in the grad_dict
+    for key in grad_dict.keys():
+
+        # If the key is not in the model's state_dict, the input grad_dict is not valid
+        if key not in model.state_dict().keys():
+            bt.logging.warning(f'Invalid grad_dict: Grad dict keys do not match the model: {key}')
+            return False
+
+        # If the corresponding value is None, the input grad_dict is not valid
+        if grad_dict[key] is None:
+            bt.logging.warning(f'Invalid grad_dict: Grad is none: {grad_dict[key]}')
+            return False
+
+        # If the value is not a float tensor, the input grad_dict is not valid
+        if not isinstance(grad_dict[key], (torch.FloatTensor, torch.cuda.FloatTensor)):
+            bt.logging.warning(f'Invalid grad_dict: Grad is not float tensor: {grad_dict[key]}')
+            return False
+
+        # If any elements of the tensor are not finite, the input grad_dict is not valid
+        if not torch.all(torch.isfinite(grad_dict[key])):
+            bt.logging.warning(f'Invalid grad_dict: Grad is not finite: {grad_dict[key]}')
+            return False
+
+        # Ensure device is correct.
+        grad_dict[key].to(model.device)
+
+        # If the shape of the tensor does not match the corresponding tensor in the model,
+        # the input grad_dict is not valid
+        if grad_dict[key].shape != model.state_dict()[key].shape:
+            bt.logging.warning(f"Invalid grad_dict: Grad dimensions do not match the model: {grad_dict[key].shape}")
+            return False
+
+    # If none of the above conditions are met, the grad_dict is valid
+    return True
+
+
+# # Tests
+# import pytest
+# import unittest
+# import tracemalloc
+# from types import SimpleNamespace
+# from unittest.mock import MagicMock, patch
+
+# class TestMergeGrads(unittest.TestCase):
+#     def setUp(self):
+#         # Set up a fake instance of your class that includes the merge_weights method
+#         self.instance = SimpleNamespace()
+
+#         # Mock the necessary attributes for the instance
+#         self.instance.metagraph = MagicMock()
+#         self.instance.dendrite = MagicMock()
+#         self.instance.model = MagicMock()
+#         self.instance.wandb = MagicMock()
+#         self.instance.device = 'cpu'
+#         self.instance.global_accumulated_ids = []
+
+#         # Set up some fake model weights
+#         self.model_weights = { 'fc1.weight': torch.randn(3, 3), 'fc2.weight': torch.randn(3, 3) }
+#         self.instance.model.state_dict.return_value = self.model_weights
+#         self.instance.model.named_parameters.return_value = [ ('fc1.weight', MagicMock()), ('fc2.weight', MagicMock()) ]
+#         for name, param in self.instance.model.named_parameters.return_value:
+#             param.grad = torch.randn(3, 3)
+    
+#     def test_merge_valid_grad_dict(self):
+
+#         # Set up the mock to return a state_dict that does not include the weights for 'fc1'
+#         self.instance.dendrite.query.return_value = SimpleNamespace( samples =  [1], grads = {'fc1.weight': torch.randn(3, 3), 'fc2.weight': torch.randn(3, 3)} )
+
+#         # Valid grad dict.
+#         _merge_grads( self.instance, None )
+
+#         # Calculate the expected averaged grads
+#         expected_grads = {
+#         'fc1.weight': torch.stack(
+#             [
+#                 self.model_weights['fc1.weight'], 
+#                 self.instance.dendrite.query().grads['fc1.weight']
+#             ]).mean(dim=0),
+#         'fc2.weight': torch.stack(
+#             [
+#                 self.model_weights['fc2.weight'], 
+#                 self.instance.dendrite.query().grads['fc1.weight']
+#             ]).sum(dim=0)
+#         }
+
+#         # Verify the model's grads were updated correctly
+#         for name, param in self.instance.model.named_parameters:
+#             self.assertTrue(torch.allclose(expected_grads[name], param.grad), msg=f"updated weights for {key} are incorrect")
+
+#     def test_merge_valid_grad_dict_multiple(self):
+
+#         # Set up the mock to return a state_dict that does not include the weights for 'fc1'
+#         self.instance.dendrite.query.return_value = [
+#             SimpleNamespace( samples = [1], grads = {'fc1.weight': torch.randn(3, 3), 'fc2.weight': torch.randn(3, 3)}),
+#             SimpleNamespace( samples = [1], grads = {'fc1.weight': torch.randn(3, 3), 'fc2.weight': torch.randn(3, 3)})
+#         ]
+
+#         # Valid grad dict.
+#         _merge_grads( self.instance, None )
+
+#         # Calculate the expected averaged grads
+#         expected_grads = {
+#         'fc1.weight': torch.stack(
+#             [
+#                 self.model_weights['fc1.weight'], 
+#                 self.instance.dendrite.query()[0].grads['fc1.weight'],
+#                 self.instance.dendrite.query()[1].grads['fc1.weight']
+
+#             ]).mean(dim=0),
+#         'fc2.weight': torch.stack(
+#             [
+#                 self.model_weights['fc2.weight'], 
+#                 self.instance.dendrite.query()[0].grads['fc2.weight'],
+#                 self.instance.dendrite.query()[1].grads['fc2.weight']
+#             ]).sum(dim=0)
+#         }
+
+#         # Verify the model's grads were updated correctly
+#         for name, param in self.instance.model.named_parameters:
+#             self.assertTrue(torch.allclose(expected_grads[name], param.grad), msg=f"updated weights for {key} are incorrect")
+
+
+#     def test_merge_valid_grad_dict_multiple_some_wrong(self):
+
+#         # Set up the mock to return a state_dict that does not include the weights for 'fc1'
+#         self.instance.dendrite.query.return_value = [
+#             SimpleNamespace( samples = [1], grads = {'fc1.weight': torch.randn(3, 3), 'fc2.weight': torch.randn(3, 3)}),
+#             SimpleNamespace( samples = [1], grads = {'fc1.weight': torch.randn(3, 3), 'fc2.weight': torch.randn(3, 3)}),
+#             SimpleNamespace( samples = [1], grads = {'fc1.weight': torch.randn(3, 4), 'fc2.weight': torch.randn(3, 3)}),
+#             SimpleNamespace( samples = [1], grads = {'fc1.weight': None, 'fc2.weight': torch.randn(3, 3)}),
+#             SimpleNamespace( samples = [1], grads = None ),
+#         ]
+
+#         # Valid grad dict.
+#         _merge_grads( self.instance, None )
+
+#         # Calculate the expected averaged grads
+#         expected_grads = {
+#         'fc1.weight': torch.stack(
+#             [
+#                 self.model_weights['fc1.weight'], 
+#                 self.instance.dendrite.query()[0].grads['fc1.weight'],
+#                 self.instance.dendrite.query()[1].grads['fc1.weight']
+
+#             ]).mean(dim=0),
+#         'fc2.weight': torch.stack(
+#             [
+#                 self.model_weights['fc2.weight'], 
+#                 self.instance.dendrite.query()[0].grads['fc2.weight'],
+#                 self.instance.dendrite.query()[1].grads['fc2.weight']
+#             ]).sum(dim=0)
+#         }
+
+#         # Verify the model's grads were updated correctly
+#         for name, param in self.instance.model.named_parameters:
+#             self.assertTrue(torch.allclose(expected_grads[name], param.grad), msg=f"updated weights for {key} are incorrect")
+
+#     def test_return_is_none(self):
+#         # Set up the mock to return a state_dict that does not include the weights for 'fc1'
+#         self.instance.dendrite.query.return_value = None
+
+#         # No valid grad dicts.
+#         with pytest.raises(Exception) as excinfo:
+#             _merge_grads( self.instance, None )
+#         assert "There are no valid gradient dicts." in str(excinfo.value)
+
+#     def test_return_is_empty(self):
+#         # Set up the mock to return a state_dict that does not include the weights for 'fc1'
+#         self.instance.dendrite.query.return_value = {}
+
+#         # No valid grad dicts.
+#         with pytest.raises(Exception) as excinfo:
+#             _merge_grads( self.instance, None )
+#         assert "There are no valid gradient dicts." in str(excinfo.value)
+
+#     def test_return_is_invalid_name(self):
+#         # Set up the mock to return a state_dict that does not include the weights for 'fc1'
+#         self.instance.dendrite.query.return_value = SimpleNamespace( samples = [1], grads = {'fc1.weight': torch.randn(3, 3), 'fc2.weightkansdsa': torch.randn(3, 3)})
+
+#         # No valid grad dicts.
+#         with pytest.raises(Exception) as excinfo:
+#             _merge_grads( self.instance, None )
+#         assert "There are no valid gradient dicts." in str(excinfo.value)
+
+#     def test_return_is_invalid_none(self):
+#         # Set up the mock to return a state_dict that does not include the weights for 'fc1'
+#         self.instance.dendrite.query.return_value = SimpleNamespace( samples = [1], grads = {'fc1.weight': torch.randn(3, 3), 'fc2.weightkansdsa': None})
+
+#         # No valid grad dicts.
+#         with pytest.raises(Exception) as excinfo:
+#             _merge_grads( self.instance, None )
+#         assert "There are no valid gradient dicts." in str(excinfo.value)
+
+#     def test_return_is_invalid_dtype(self):
+#         # Set up the mock to return a state_dict that does not include the weights for 'fc1'
+#         self.instance.dendrite.query.return_value = SimpleNamespace( samples = [1], grads = {'fc1.weight': torch.randint(0, 10, (3, 3), dtype=torch.int64), 'fc2.weightkansdsa':  torch.randint(0, 10, (3, 3), dtype=torch.int64)})
+
+#         # No valid grad dicts.
+#         with pytest.raises(Exception) as excinfo:
+#             _merge_grads( self.instance, None )
+#         assert "There are no valid gradient dicts." in str(excinfo.value)
+
+#     def test_invalid_dimension(self):
+#         # Set up the mock to return a state_dict that does not include the weights for 'fc1'
+#         self.instance.dendrite.query.return_value = SimpleNamespace( samples = [1], grads = {'fc1.weight': torch.randn(3, 4), 'fc2.weight': torch.randn(3, 3)} )
+
+#         # No valid grad dicts.
+#         with pytest.raises(Exception) as excinfo:
+#             _merge_grads( self.instance, None )
+#         assert "There are no valid gradient dicts." in str(excinfo.value)
+
+#     def test_samples_accumulate(self):
+#         # Set up the mock to return a state_dict that does not include the weights for 'fc1'
+#         self.instance.dendrite.query.return_value = [
+#                 SimpleNamespace( samples = [1], grads = {'fc1.weight': torch.randn(3, 3), 'fc2.weight': torch.randn(3, 3)} ),
+#                 SimpleNamespace( samples = [1], grads = {'fc1.weight': torch.randn(3, 3), 'fc2.weight': torch.randn(3, 3)} ),
+#                 SimpleNamespace( samples = [1], grads = {'fc1.weight': torch.randn(3, 3), 'fc2.weight': torch.randn(3, 3)} ),
+#                 SimpleNamespace( samples = [1], grads = {'fc1.weight': torch.randn(3, 3), 'fc2.weight': torch.randn(3, 3)} ),
+#                 SimpleNamespace( samples = [1], grads = {'fc1.weight': torch.randn(3, 3), 'fc2.weight': torch.randn(3, 3)} ),
+#                 SimpleNamespace( samples = [1], grads = {'fc1.weight': torch.randn(3, 3), 'fc2.weight': torch.randn(3, 3)} ),
+#             ]
+
+#         # Merge grads.
+#         _merge_grads( self.instance, None )
+
+#         # Check that the samples have accumulated
+#         assert len( self.instance.global_accumulated_ids ) == 6
+
+    
+# if __name__ == "__main__":
+#     unittest.main()
