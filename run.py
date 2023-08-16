a41c209ae2f43d190591ccc55549989a71bbdbe8
unconst <jake@bittensor.com>
2023-08-16 12:14:23 -0500

    increase spec version

--- run.py
+++ run.py
@@ -15,7 +15,7 @@
 # OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
 # DEALINGS IN THE SOFTWARE.
 
-__spec_version__ = '0.0.1'
+__spec_version__ = '0.0.2'
 
 import os
 import sys
