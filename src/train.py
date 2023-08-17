afd429e2d328002280a5e3f0f436772af7a256cc
unconst <jake@bittensor.com>
2023-08-17 10:36:14 -0500

    increase eval rate

--- src/train.py
+++ src/train.py
@@ -33,7 +33,7 @@ def get_config():
     parser.add_argument( '--accs_per_step', type=int, default = 5, help = 'Number of training accumulation steps.')
     parser.add_argument( '--steps_per_log', type=int, default = 1, help = 'Number of steps per log.')
     parser.add_argument( '--steps_per_sync', type=int, default = 100, help = 'Number of steps per chain sync.')
-    parser.add_argument( '--steps_per_eval', type=int, default = 300, help = 'Number of steps per eval.')
+    parser.add_argument( '--steps_per_eval', type=int, default = 50, help = 'Number of steps per eval.')
     parser.add_argument( '--blocks_per_reduce', type=int, default = 10_000, help = 'Number of steps reduce.')
     parser.add_argument( '--blocks_per_set_weights', type=int, default = 100, help = 'Number of blocks before we set weights.')
     parser.add_argument( '--num_warmup', type=int, default = 2000, help = 'Scheduler warm up steps.')
